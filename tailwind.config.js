b255cd71314ac77cd8a8765375b5c1a4341d1dbb
hav0kinho <ruallysonfelype@gmail.com>
2024-06-19 12:07:58 -0300

    Configuracao dos estilos, fontes e variaveis de cores do tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,12 +4,15 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        "bg-color": "#FFECC9",
+        "primary-light": "#FFECC9",
         "primary-white": "#FFFFFF",
         "primary-orange": "#F66F4D",
         "primary-blue": "#2E476B",
         "primary-gray": "#5B5F62",
         "primary-black": "#2D3134",
+      },
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
       },
     },
   },
