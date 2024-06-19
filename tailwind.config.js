f8cbfd152fdcd2928f310a3f2ce473eae6bf3ca5
hav0kinho <ruallysonfelype@gmail.com>
2024-06-19 10:48:36 -0300

    Configuração do tema tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "bg-color": "#FFECC9",
+        "primary-white": "#FFFFFF",
+        "primary-orange": "#F66F4D",
+        "primary-blue": "#2E476B",
+        "primary-gray": "#5B5F62",
+        "primary-black": "#2D3134",
+      },
+    },
   },
   plugins: [],
 };
