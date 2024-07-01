6783564ed4b697b62df1ea4c40c24f5e811669ae
hav0kinho <ruallysonfelype@gmail.com>
2024-07-01 12:00:58 -0300

    Mobile: Melhoria na responsividade do Footer | Ajustes no Main/Contact

--- src/components/ContactEmail/index.tsx
+++ src/components/ContactEmail/index.tsx
@@ -11,7 +11,7 @@ const ContactEmail = () => {
           className="w-[490px] h-[60px] py-6 px-10 rounded-xl  mr-6 max-sm:w-full max-sm:mr-0 max-sm:text-center"
         />
         <button className="w-[60px] h-[60px] bg-primary-orange rounded-xl max-sm:w-full max-sm:flex max-sm:items-center max-sm:justify-center max-sm:mt-3">
-          <strong className="max-sm:text-2xl max-sm:text-white">
+          <strong className="hidden max-sm:inline max-sm:text-2xl max-sm:text-white">
             Send Mail
           </strong>
           <img src={PaperPlane} alt="Avião de papel" className="" />
--- src/components/SocialButton/index.tsx
+++ src/components/SocialButton/index.tsx
@@ -8,20 +8,27 @@ type Props = {
 
 const SocialButton = ({ logo }: Props) => {
   let logoImg = "";
+  let size = "4";
   switch (logo) {
     case "facebook":
       logoImg = Facebook;
       break;
     case "instagram":
       logoImg = Instragram;
+      size = "6";
       break;
     case "twitter":
       logoImg = Twitter;
+      size = "6";
       break;
   }
   return (
-    <button className="w-8 h-8 rounded-full border-[1px] border-primary-black">
-      <img src={logoImg} alt={`${logo} logo`} className="mx-auto" />
+    <button className="w-8 h-8 rounded-full border-[1px] border-primary-black max-sm:w-16 max-sm:h-16">
+      <img
+        src={logoImg}
+        alt={`${logo} logo`}
+        className={`mx-auto max-sm:w-${size}`}
+      />
     </button>
   );
 };
